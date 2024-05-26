793c8b75fcf31712c4fd3c9cb2b188f74c95f0bf
carolinac <carol.dev.ve@gmail.com>
2024-05-26 14:25:52 -0400

    add label to component

--- src/components/Layout/CardsLayout.tsx
+++ src/components/Layout/CardsLayout.tsx
@@ -9,7 +9,7 @@ const CardsLayout : React.FC  = ({cardsData}: Props) => {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {cardsData.map(data => <Card title={data.fuel} number={data.perc}/>)}
+        {cardsData.map(data => <Card key={data.fuel} title={data.fuel} number={data.perc} label={data.label}/>)}
       </div>
     </div>
   );
