70f608ef52045259995226c05ea49aa39b681690
carolinac <carol.dev.ve@gmail.com>
2024-05-26 15:50:53 -0400

    fix layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-blue-600 text-white p-4">
+      <header className="bg-gray-800 text-white p-4">
         <h1 className="text-2xl">UK Energy Mix</h1>
       </header>
 
