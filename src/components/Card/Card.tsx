0e63e363f94de63eeabfb67c21b339b755c1daa0
carolinac <carol.dev.ve@gmail.com>
2024-05-26 14:26:11 -0400

    add styles to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
-
+import { MdWbSunny, MdOutdoorGrill, MdWarning, MdWindPower, MdWater, MdOilBarrel, MdForest, MdMoney, MdBolt } from "react-icons/md";
 interface CardProps {
   title: string;
   number: number;
+  label?:string
 }
 
-const Card: React.FC<CardProps> = ({ title, number }) => {
+// Mapping Icons For Card Exclusively for Carbon Intensity
+const iconSize = 20
+const mappingIcon = {
+  solar: <MdWbSunny color="yellow" size={iconSize} />,
+  coal: <MdOutdoorGrill color="brown" size={iconSize} />,
+  nuclear: <MdWarning color="green" size={iconSize} />,
+  wind: <MdWindPower color="blue" size={iconSize} />,
+  gas: <MdOilBarrel color="grey" size={iconSize} />,
+  hydro: <MdWater color="blue" size={iconSize} />,
+  imports: <MdMoney color="brown" size={iconSize} />,
+  biomass: <MdForest color='green'size={iconSize}  />,
+  other: <MdBolt color="black" size={iconSize} />
+}
+
+const backgroundCard = {
+  biggest: 'bg-sky-100',
+  lowest: 'bg-red-100'
+}
+
+const Card: React.FC<CardProps> = ({ title, number , label }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-6 flex flex-col items-center justify-center">
-      <div className="text-gray-500 text-sm">{title}</div>
-      <div className="text-3xl font-bold">{number}</div>
+    <div className ={` ${Object.keys(backgroundCard).includes(label)? backgroundCard[label] : 'bg-white'} shadow-md rounded-lg p-6 flex flex-col items-center justify-center`}>
+      <div className="w-[100%] flex justify-between">
+        <div className="text-gray-500 text-sm text-left uppercase">{title}</div>
+        <div>{Object.keys(mappingIcon).includes(title) ? mappingIcon[title] : mappingIcon} </div>
+      </div>
+      <div className="text-3xl font-bold">{number}%</div>
+
     </div>
   );
 };
