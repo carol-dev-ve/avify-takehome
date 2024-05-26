aa42b055a1542ed8b6065d4ec27d895ed43f2c5d
carolinac <carol.dev.ve@gmail.com>
2024-05-26 14:26:40 -0400

    consider loading and error data from useQuery

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../components/Layout/Layout'
 import { useQuery } from '@tanstack/react-query'
 import { fetchCarbonIntensity } from '../api/carbonintensity/carbonintensity'
+import CardsLayout from '../components/Layout/CardsLayout'
+import Loading from '../components/ui/Loading'
+import ErrorComponent from '../components/ui/Error'
+import { orderGenerationMix } from '../helpers/orderCarbonData'
 
 const Home = () => {
 
-    const queryCarbonData = useQuery({
-        queryKey: ['carbonIntensity'],
-        queryFn:()=>fetchCarbonIntensity()
-    })
+  const queryCarbonData = useQuery({
+    queryKey: ['carbonIntensity'],
+    queryFn: fetchCarbonIntensity
+  })
 
-    console.log(queryCarbonData)
 
   return (
-    <Layout>Home Test</Layout>
+    <Layout>
+      {queryCarbonData.isLoading && <Loading />}
+      {queryCarbonData.error && <ErrorComponent message="Error getting data" />}
+      {!queryCarbonData.isLoading && !queryCarbonData.error && <CardsLayout cardsData={orderGenerationMix(queryCarbonData?.data?.data?.generationmix || [])} />}
+    </Layout>
   )
 }
 
