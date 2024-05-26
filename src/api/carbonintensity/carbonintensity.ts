d337fa8c9d8bdfdcc7b9d8d0ac985ffad124c647
carolinac <carol.dev.ve@gmail.com>
2024-05-26 14:18:46 -0400

    api to fetch

--- src/api/carbonintensity/carbonintensity.ts
+++ src/api/carbonintensity/carbonintensity.ts
@@ -2,11 +2,8 @@ import axios from "axios"
 import { DataCarbonResponse } from "../../interfaces/CarbonDataResponse"
 
 export const fetchCarbonIntensity = async (): Promise<DataCarbonResponse> => {
-    console.log("aqui")
     try {
-        console.log("entro")
         const response = await axios.get("https://api.carbonintensity.org.uk/generation")
-        console.log(response)
         return response.data
     } catch(error) {
         throw error(error.message || 'Error getting carbon intensity information')
