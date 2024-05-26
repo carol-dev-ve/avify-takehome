f02fdc23940dab93f84c5936cf7b19b138f271c8
carolinac <carol.dev.ve@gmail.com>
2024-05-26 15:49:50 -0400

    fix card layout

--- src/components/Layout/CardsLayout.tsx
+++ src/components/Layout/CardsLayout.tsx
@@ -3,13 +3,13 @@ import Card from '../Card/Card';
 import { Generationmix } from '../../interfaces/CarbonDataResponse';
 
 interface Props {
-    cardsData: Generationmix[]
+  cardsData: Generationmix[]
 }
-const CardsLayout : React.FC  = ({cardsData}: Props) => {
+const CardsLayout: React.FC = ({ cardsData }: Props) => {
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className=" bg-gray-100 p-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {cardsData.map(data => <Card key={data.fuel} title={data.fuel} number={data.perc} label={data.label}/>)}
+        {cardsData.map(data => <Card key={data.fuel} title={data.fuel} number={data.perc} label={data.label} />)}
       </div>
     </div>
   );
