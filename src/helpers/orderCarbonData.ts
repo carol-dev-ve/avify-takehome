7671efd36bf4e0ace0d6323cbba4af43a580da94
carolinac <carol.dev.ve@gmail.com>
2024-05-26 14:25:34 -0400

    fix function to indicate biggest or lowest for all with the same percentage

--- src/helpers/orderCarbonData.ts
+++ src/helpers/orderCarbonData.ts
@@ -1,17 +1,26 @@
-import { DataCarbonResponse, Generationmix } from "../interfaces/CarbonDataResponse";
+import { Generationmix } from "../interfaces/CarbonDataResponse";
 
-  const orderGenerationMix(data: DataCarbonResponse): Generationmix[] => {
-    const sortedData = data.data.generationmix.sort((a, b) => b.perc - a.perc);
+export const orderGenerationMix = (data: Generationmix[]): Generationmix[] => {
+  const sortedData = [...data].sort((a, b) => b.perc - a.perc);
 
-    if (sortedData.length > 0) {
-      sortedData[0].label = 'biggest';
-      sortedData[sortedData.length - 1].label = 'lowest';
-    }
-
-    if (sortedData.length > 1) {
-      sortedData[1].label = 'second biggest';
-      sortedData[sortedData.length - 2].label = 'second lowest';
-    }
-
-    return sortedData;
+  if (sortedData.length > 0) {
+    sortedData[0].label = 'biggest';
+    sortedData[sortedData.length - 1].label = 'lowest';
   }
+
+
+  let maxPerc = sortedData[0].perc;
+  let minPerc = sortedData[sortedData.length - 1].perc;
+
+  // Find all items with the same maximum percentage and label them as "biggest"
+  for (let i = 1; i < sortedData.length && sortedData[i].perc === maxPerc; i++) {
+    sortedData[i].label = 'biggest';
+  }
+
+  // Find all items with the same minimum percentage and label them as "lowest"
+  for (let i = sortedData.length - 2; i >= 0 && sortedData[i].perc === minPerc; i--) {
+    sortedData[i].label = 'lowest';
+  }
+
+  return sortedData;
+};
